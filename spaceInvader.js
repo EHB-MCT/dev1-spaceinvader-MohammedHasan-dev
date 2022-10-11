6c18e4da61b1392299f293b274143f2fd41ccff1
Mohammed Hasan <mohammed.hasan@student.ehb.be>
2022-10-11 21:56:33 +0100

    Avatar

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,51 @@
-"use strict";
 
-alert("Draw your space invader here");
+    "use strict";
+
+let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    context.beginPath();
+    context.fillStyle = "black";
+    context.rect(200,200,300,300);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = "#66FF00";
+    context.rect(325,210,50,50);
+
+    context.rect(325,320,50,50)
+
+    context.rect(325,370,50,50);
+
+    context.rect(325,420,50,50);
+
+    context.rect(375,420,50,50);
+
+    context.rect(425,420,50,50);
+
+    context.rect(425,375,50,50);
+
+    context.rect(275,420,50,50);
+
+    context.rect(225,420,50,50);
+
+    context.rect(225,375,50,50);
+
+    context.fill();
+
+    
+    
+    
+
+   
+
+
+
+
+
+
+
+
+
